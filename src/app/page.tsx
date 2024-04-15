c3e15dd797eea59eead83422c6743ca1e9dabfcf
Denys Horkavyi <denisdenis2295@gmail.com>
2024-04-15 18:47:47 +0200

    feat/add hero slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import Header from '@/components/Header/Header';
+import HeroSlider from '@/components/Hero/HeroSlider';
+
+const slides = [
+  {
+    src: 'https://picsum.photos/seed/img5/600/400',
+    alt: 'Image 1 for carousel',
+  },
+  {
+    src: 'https://picsum.photos/seed/img6/600/400',
+    alt: 'Image 2 for carousel',
+  },
+  {
+    src: 'https://picsum.photos/seed/img7/600/400',
+    alt: 'Image 3 for carousel',
+  },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24 ">
       <div className="text-white">
-        <h2>Elektychna fortecia</h2>
+        <HeroSlider data={slides} />
       </div>
     </main>
   );
