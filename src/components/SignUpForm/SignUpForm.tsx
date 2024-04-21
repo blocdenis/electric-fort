70347e729048bd93c819c40937fd3ae0e3d5cffc
Denys Horkavyi <denisdenis2295@gmail.com>
2024-04-21 20:42:26 +0200

    feat/add conditional rendering into modal auth

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -12,10 +12,12 @@ type Inputs = {
 };
 
 export default function SignUpForm() {
+  const [isRegistration, setIsRegistration] = useState(false);
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePasswordVisiblity = () => {
     setPasswordShown((passwordShown) => !passwordShown);
   };
+
   const {
     register,
     handleSubmit,
@@ -26,13 +28,17 @@ export default function SignUpForm() {
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
     reset();
+    //fetch POST
   };
 
   return (
     <div className={styles.form_container}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Номер телефону</label>
-        <input placeholder="+380 (__)___-__-__" {...register('phone')} />
+        <input
+          placeholder="+380 (__)___-__-__"
+          {...(register('phone'), { required: true })}
+        />
         <div className={styles.password_container}>
           <label>Пароль</label>
           <input
@@ -47,39 +53,66 @@ export default function SignUpForm() {
             onClick={togglePasswordVisiblity}
           />
         </div>
-        <div className={styles.password_container}>
-          <label>Повторити пароль</label>
-          <input
-            placeholder="Пароль (не менше 6 символів)"
-            type={passwordShown ? 'text' : 'password'}
-            {...register('confirm_password', {
-              // required: true,
-              validate: (val: string) => {
-                if (watch('password') !== val) {
-                  return 'Your passwords do not match';
-                }
-              },
-            })}
-          />
-          <EyeIcon
-            className={styles.eye_icon}
-            onClick={togglePasswordVisiblity}
-          />
-        </div>
-        <div className={styles.policy_container}>
-          {' '}
-          <input type="checkbox" />{' '}
-          <Link href="/return_policy">
-            Я приймаю умови користувальницької угоди
-          </Link>
-        </div>
+        {!isRegistration && (
+          <div className={styles.policy_container}>
+            {' '}
+            <input type="checkbox" />{' '}
+            <div className={styles.text_container}>
+              <span>Запам’ятати мене</span>
+              <span>Забули пароль?</span>
+            </div>
+          </div>
+        )}
+        {isRegistration && (
+          <>
+            <div className={styles.password_container}>
+              <label>Повторити пароль</label>
+              <input
+                placeholder="Повторити пароль"
+                type={passwordShown ? 'text' : 'password'}
+                {...register('confirm_password', {
+                  required: true,
+                  validate: (val: string) => {
+                    if (watch('password') !== val) {
+                      return 'Your passwords do not match';
+                    }
+                  },
+                })}
+              />
+              <EyeIcon
+                className={styles.eye_icon}
+                onClick={togglePasswordVisiblity}
+              />
+            </div>
+            <div className={styles.policy_container}>
+              {' '}
+              <input type="checkbox" />{' '}
+              <Link href="/return_policy">
+                Я приймаю умови користувальницької угоди
+              </Link>
+            </div>
+          </>
+        )}
 
         {errors.password && (
           <span className={styles.error_message}>Це поле обовязкове</span>
         )}
-        <input type="submit" value="Зареєструватися" />
+        <input
+          type="submit"
+          value={isRegistration ? 'Зареєструватися' : 'Увійти'}
+        />
       </form>
-      <button className={styles.btn}>Увійти</button>
+      {!isRegistration && (
+        <button className={styles.btn} onClick={() => setIsRegistration(true)}>
+          Зареєструватися
+        </button>
+      )}
+      {isRegistration && (
+        <button className={styles.btn} onClick={() => setIsRegistration(false)}>
+          Увійти
+        </button>
+      )}
+
       <div className={styles.seperate_container}>
         <div className={styles.line}></div>
         <span>або</span>
