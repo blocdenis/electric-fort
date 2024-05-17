282c2ec2d27625efaa23a8e1ed27bd0a2cf74407
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-05-17 16:09:50 +0300

    added policy edited return policy

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -5,7 +5,7 @@ function Text({
   children,
   className,
 }: PropsWithChildren<{ className?: string }>) {
-  return <p className={classNames(' text-base mb-5', className)}>{children}</p>;
+  return <p className={classNames(' text-base', className)}>{children}</p>;
 }
 
 export default Text;
