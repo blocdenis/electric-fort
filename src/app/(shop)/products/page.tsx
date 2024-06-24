bc8d594134c16aacf4d774a87461eb36ab11ed40
Denys Horkavyi <denisdenis2295@gmail.com>
2024-06-24 19:49:49 +0200

    fix suspense

--- src/app/(shop)/products/page.tsx
+++ src/app/(shop)/products/page.tsx
@@ -5,6 +5,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Breadcrumbs from '@/components/Breadcrumb/Breadcrumbs';
 
+// Функция для получения данных продуктов с API
 const fetchProducts = async (query: string) => {
   const { data } = await axios.get(
     `https://2qtsbt2v-80.euw.devtunnels.ms/api/get/Product?all_data=true&field=name&search=${query}&equal=false&pagination=true&page_size=25&page=1`
@@ -13,37 +14,47 @@ const fetchProducts = async (query: string) => {
   return data;
 };
 
-const ProductsList: React.FC = () => {
+const ProductsList: React.FC<{ query: string }> = ({ query }) => {
+  const { data } = useQuery({
+    queryKey: ['products', query],
+    queryFn: () => fetchProducts(query),
+    enabled: !!query,
+  });
+
+  const products = data?.data || [];
+  return (
+    <div>
+      <h1>Search results</h1>
+      {products.length > 0 ? (
+        <ul>
+          {products.map((product: any) => (
+            <li key={product.id}>{product.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No products found</p>
+      )}
+    </div>
+  );
+};
+
+const ProductsPage: React.FC = () => {
   const links = [{ name: 'Результати пошуку' }];
   const searchParams = useSearchParams();
   const q = searchParams.get('q');
 
-  const { data } = useQuery({
-    queryKey: ['products', q],
-    queryFn: () => fetchProducts(q || ''),
-    enabled: !!q,
-  });
+  if (!q) {
+    return <p>Please provide a search query</p>;
+  }
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error loading products</div>;
-  const products = data?.data || [];
   return (
-    <Suspense fallback={<>Loading</>}>
-      <div>
-        <Breadcrumbs items={links} />
-        <h1>Search results</h1>
-        {products?.length > 0 ? (
-          <ul>
-            {products?.map((product: any) => (
-              <li key={product.id}>{product.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No products found</p>
-        )}
-      </div>
-    </Suspense>
+    <div>
+      <Breadcrumbs items={links} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductsList query={q} />
+      </Suspense>
+    </div>
   );
 };
 
-export default ProductsList;
+export default ProductsPage;
