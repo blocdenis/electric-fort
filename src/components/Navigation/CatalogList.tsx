573eb8f9b4f91788acc410b4b4228758dff816a5
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-07-01 13:39:12 +0300

    change revalidation time , delete unused imports

--- src/components/Navigation/CatalogList.tsx
+++ src/components/Navigation/CatalogList.tsx
@@ -1,16 +1,10 @@
-// 'use client';
-// import { categories } from '@/lib/db/categories';
 import React from 'react';
 import CatalogItem from './CatalogItem';
-import { Brand, Category } from '@/lib/types/types';
-import { useQuery } from '@tanstack/react-query';
 import { getAllBrands, getAllCategories } from '@/services/api/api';
-import { categories } from '@/lib/db/categories';
-import { brands } from '@/lib/db/brands';
 
 async function CatalogList() {
-  const categories = await getAllCategories({ next: { revalidate: 3600 } });
-  const brands = await getAllBrands({ next: { revalidate: 3600 } });
+  const categories = await getAllCategories({ next: { revalidate: 60 } });
+  const brands = await getAllBrands({ next: { revalidate: 60 } });
 
   return (
     <ul className=" bg-backgroung">
