ccc98cac1f4ca0b77230e32e7f1cab051a5d0d4b
Denys Horkavyi <denisdenis2295@gmail.com>
2024-07-17 17:08:53 +0200

    fix pickupmethod

--- src/app/(shop)/order/page.tsx
+++ src/app/(shop)/order/page.tsx
@@ -4,20 +4,23 @@ import React from 'react';
 import OrderForm from '../../../components/Order/OrderForm';
 import './styles.scss';
 import OrderCart from '@/components/Order/OrderCart';
+import Container from '@/components/Container/Container';
 
 const OrderPage = () => {
   const breadcrumbsItens = [{ name: 'Оформлення замовлення', href: '/order' }];
   return (
     <div>
-      <Breadcrumbs items={breadcrumbsItens} />
-      <div className="container">
-        <div className="item">
-          <OrderForm />
+      <Container>
+        <Breadcrumbs items={breadcrumbsItens} />
+        <div className="container">
+          <div className="item">
+            <OrderForm />
+          </div>
+          <div className="item">
+            <OrderCart />
+          </div>
         </div>
-        <div className="item">
-          <OrderCart />
-        </div>
-      </div>
+      </Container>
     </div>
   );
 };
