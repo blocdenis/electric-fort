83a3151c3b287dfe2fc12b58d96a00d51932446e
Denys Horkavyi <denisdenis2295@gmail.com>
2024-07-29 10:55:51 +0200

    filter provider

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -4,6 +4,7 @@ import { ReactNode, useState } from 'react';
 import { AuthProvider } from './AuthContext';
 import { ShoppingCartProvider } from './ShoppingCartContext';
 import { FavoritesProvider } from './FavoritesContext';
+import { FiltersProvider } from './FiltersContext';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Provider({ children }: { children: ReactNode }) {
