9576518a7f6bdff7cd573f3de9d1f9a26bd13025
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-08-04 20:16:21 +0300

    add bg img to delivery page desktop

---
+++ src/components/Delivery/DeliveryBottomSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Delivery.module.scss';
+
+function DeliveryBottomSection() {
+  return (
+    <section className={styles.delivery_bottom}>
+      <div className={styles.delivery_bottom_img}></div>
+    </section>
+  );
+}
+
+export default DeliveryBottomSection;
