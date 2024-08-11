b3e76643e77f97c4ff13d5d88122762dbf73a29e
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-08-11 21:43:21 +0300

    add privat route

--- src/app/(shop)/user_profile/page.tsx
+++ src/app/(shop)/user_profile/page.tsx
@@ -1,24 +1,31 @@
+'use client';
 import Breadcrumbs from '@/components/Breadcrumb/Breadcrumbs';
 import LogOutButton from '@/components/Buttons/LogOutButton/LogOutButton';
 import Container from '@/components/Container/Container';
+import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
 import Profile from '@/components/Profile/Profile';
 import Section from '@/components/Section/Section';
-import SectionTitle from '@/components/Section/SectionTitle/SectionTitle';
 import styles from '@/components/Section/SectionTitle/SectionTitle.module.scss';
+import { useAuth } from '@/context/AuthContext';
 
 function Page() {
   const links = [{ name: 'Особистий кабінет' }];
+  const { isAuthenticated } = useAuth();
   return (
-    <Container>
-      <Breadcrumbs items={links} />
-      <Section className="px-9">
-        <div className=" flex justify-between mb-[19px]">
-          <h2 className={styles.section_heading_profile}>Особистий кабінет</h2>
-          <LogOutButton />
-        </div>
-        <Profile />
-      </Section>
-    </Container>
+    <PrivateRoute isAuth={isAuthenticated}>
+      <Container>
+        <Breadcrumbs items={links} />
+        <Section className="px-9">
+          <div className=" flex justify-between mb-[19px]">
+            <h2 className={styles.section_heading_profile}>
+              Особистий кабінет
+            </h2>
+            <LogOutButton />
+          </div>
+          <Profile />
+        </Section>
+      </Container>
+    </PrivateRoute>
   );
 }
 
---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface PrivateRouteProps {
+  children: ReactNode;
+  isAuth: boolean;
+}
+
+const PrivateRoute = ({ children, isAuth }: PrivateRouteProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.push('/#auth'); // redirect to /#auth if user not authenticate
+    }
+  }, [isAuth, router]);
+
+  // render children only if authenticate
+  return isAuth ? children : null;
+};
+
+export default PrivateRoute;
