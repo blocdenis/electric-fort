535146cb5b2e9d36c5dabea653012acc5a5e0c6c
MViktoriia <88782163+MViktoriia@users.noreply.github.com>
2024-09-11 16:19:08 +0300

    added trim func removed sticky on desctop

--- src/lib/utils/formatString.ts
+++ src/lib/utils/formatString.ts
@@ -1,6 +1,7 @@
 //from uppereCase to lowerCase with first upperCase letter
 export const formatedString = (string: string) => {
   const formatedString =
-    string.charAt(0).toLocaleUpperCase() + string.slice(1).toLocaleLowerCase();
+    string.trim().charAt(0).toLocaleUpperCase() +
+    string.trim().slice(1).toLocaleLowerCase();
   return formatedString;
 };
